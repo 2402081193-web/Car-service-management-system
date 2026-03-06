32280a34ad298897e05464d31165b4541e00b11e
2402081193 <2402081193@siswa.icats.edu.my>
2026-03-06 16:49:38 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
+// Firebase 配置
 const firebaseConfig = {
-  apiKey: "AIzaSyAKVNXmMefJWmBfqiKKj3fKHBWaSdKw4LA",
-  authDomain: "car-service-managemnt-system.firebaseapp.com",
-  projectId: "car-service-managemnt-system",
-  storageBucket: "car-service-managemnt-system.firebasestorage.app",
-  messagingSenderId: "593184640451",
-  appId: "1:593184640451:web:1bc4a0063eaa8c1b0ec176"
+    apiKey: "AIzaSyAKVNXmMefJWmBfqiKKj3fKHBWaSdKw4LA",
+    authDomain: "car-service-managemnt-system.firebaseapp.com",
+    projectId: "car-service-managemnt-system",
+    storageBucket: "car-service-managemnt-system.firebasestorage.app",
+    messagingSenderId: "593184640451",
+    appId: "1:593184640451:web:1bc4a0063eaa8c1b0ec176",
+    measurementId: "G-MF85T06LC1"
 };
 
-export const app = initializeApp(firebaseConfig);
+// 初始化 Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+// 管理员注册密码（可以修改）
+const ADMIN_REGISTER_PASSWORD = "ADMIN2024";
+
+// 导出全局变量
+window.auth = auth;
+window.db = db;
+window.ADMIN_REGISTER_PASSWORD = ADMIN_REGISTER_PASSWORD;
